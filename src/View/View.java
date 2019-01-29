9a58a1afc2ec032c7ee90b0668ba120daa40b046
sasa <sanmoh99@gmail.com>
2019-01-29 15:20:06 +0330

    improved menu

--- src/View/View.java
+++ src/View/View.java
@@ -10,28 +10,40 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.File;
 import java.util.Random;
 
 
 public class View extends Application {
     public Stage primaryStage;
+    private static Media sound = new Media(new File("menu.mp3").toURI().toString());
+    private static MediaPlayer mediaPlayer = new MediaPlayer(sound);
 
     private void debug() {
         CommandController.commandController.run("Level0");
     }
-
+    {
+        if(sound == null)
+            System.out.println("NULLLL");
+        System.out.println("YAYY");
+        mediaPlayer.play();
+    }
     @Override
     public void start(Stage primaryStage) throws Exception{
         this.primaryStage = primaryStage;
         debug();
         MenuScene.menuScene.setPrimaryStage(primaryStage);
         MenuScene.menuScene.start();
+
+        String musicFile = "menu.mp3";     // For example
     }
 
 
